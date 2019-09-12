c44e16167fac227d8acf5330bc704bfecd845c7f
Van <vanrepositorygroup@gmail.com>
2019-09-12 06:50:49 -0300

    Subindo codigo de webcam

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const http = require('http').createServer(app);
 const path = require('path');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const dateFormat = require('dateformat');
+const io = require('socket.io')(http);
+const cv = require('opencv4nodejs');
 
+const wCap = new cv.VideoCapture(0)
 var now = new Date();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev')); // registrar cada pedido para o console
+
 
 
 // rotas ===================================
@@ -16,11 +21,19 @@ require('./config/router.js')(app);
 
 // Rotas nodo_modules e views ===============
 app.use(express.static(path.join(__dirname, 'node_modules')));
-app.set('viwes', path.join(__dirname, 'app/views'));
-app.set('views engine', 'ejs');
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'ejs'); //configura ejs como template
 
 //subir o banco 
 http.listen(8042);
 console.log('A magia acontece na porta 8042');
+
+//WEBCAM ======================================
+// setInterval(() => {
+// io.emit('image', 'some data')
+// },1000)
+
+
+
 
 exports = module.exports = app;
--- app/controller/Home.js
+++ app/controller/Home.js
@@ -1,5 +1,25 @@
 
 exports.home = ((req, res) => {
-    console.log('passei aqui');
-    res.send('funciona');
-})
+    res.render('index',{
+        
+    });
+
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+        socket.on('image', function (data) {
+            console.log('data', data);
+            const image = document.getElementById('image');
+            image.src = `data:iamge/jpeg;base64,${image}`
+             
+            setInterval(() => {
+                const frame = wCap.read();
+                const image = cv.imencode('.jpg', frame).toString('base64');
+                io.emit('image', 'some data')
+            }, 1000)
+        });
+    });
+});
+
